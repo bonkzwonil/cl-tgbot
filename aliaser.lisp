315c6679392df6a43cf6c399d9cef879bb81f8b6
Mathias Menzel-Nielsen <mnzl@kackbook.lan>
2016-01-21 21:02:42 +0100

    fix

--- aliaser.lisp
+++ aliaser.lisp
@@ -26,67 +26,6 @@
 
 
 
-
-;; Dummy Store
-(defparameter *storage* (make-hash-table :test 'equalp))
-
-(defun store (key value)
-  (setf (gethash key *storage*) value))
-
-(defun restore (key)
-  (gethash key *storage*))
-
-
-;; Btree storage
-(require :cl-btree-0.5)
-(b-tree:open #p"data.dat" :if-does-not-exist :create "~{~A~^~}" words))
-
-(defun build (words)
-  (join-words (mapcar #'(lambda (word) (char word 0))
-	  words)))
-  
-(defun save (text)
-  (let ((alias (build (cl-ppcre:split " " text))))
-    (store alias text)))
-
-
-(defun lookup (alias)
-  (restore alias))
-
-
-
-
-
-;; Dummy Store
-(defparameter *storage* (make-hash-table :test 'equalp))
-
-(defun store (key value)
-  (setf (gethash key *storage*) value))
-
-(defun restore (key)
-  (gethash key *storage*))
-
-
-;; Btree storage
-(require :cl-btree-0.5)
-(b-tree:open #p"data.dat" :if-does-not-exist :create "~{~A~^~}" words))
-
-(defun build (words)
-  (join-words (mapcar #'(lambda (word) (char word 0))
-	  words)))
-  
-(defun save (text)
-  (let ((alias (build (cl-ppcre:split " " text))))
-    (store alias text)))
-
-
-(defun lookup (alias)
-  (restore alias))
-
-
-
-
-
 ;; Dummy Store
 (defparameter *storage* (make-hash-table :test 'equalp))
 
